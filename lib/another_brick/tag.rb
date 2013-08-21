1ad126908b2d1f0fc2831e24a056b1e9263f6054
Ricardo Ruiz <ricardo.ruiz@locaweb.com.br>
2013-08-20 21:34:32 -0300

    versioning from ALL the tags

--- lib/another_brick/tag.rb
+++ lib/another_brick/tag.rb
@@ -2,27 +2,34 @@ module AnotherBrick
   module Tag
     extend self
 
+    PREFIXES = %w(unstable stable testing)
+
     def create(prefix)
-      version_tag = get_last_tag('v')
+      major, minor, build = last_tag ? strip_version(last_tag) : [0, 0, 0]
 
-      major, minor = strip_version('v', version_tag)
-
-      last_tag = get_last_tag("#{prefix}_#{major}.#{minor}")
-
-      build = last_tag ? strip_version("#{prefix}_", last_tag).last + 1 : 0
-
-      "#{prefix}_#{major}.#{minor}.#{build}".tap do |next_tag|
+      "#{prefix}_#{major}.#{minor}.#{build.to_i + 1}".tap do |next_tag|
         `git tag #{next_tag}`
         `git push --tags`
       end
     end
 
-    def get_last_tag(pattern)
-      `git tag | grep ^#{pattern} | sort -V`.lines.map(&:chomp).last
+    def version_sorter
+      proc do |v1, v2|
+        Gem::Version.new(v1) <=> Gem::Version.new(v2)
+      end
     end
 
-    def strip_version(prefix, version)
-      version.delete(prefix).split('.').map(&:to_i)
+    def last_tag
+      prefix_re = /(?:v|#{PREFIXES.map { |e| "#{e}_" }.join("|") })(\d+\.\d+(?:\.\d+)?)/
+      `git tag `.lines.map(&:chomp).select { |e| e =~ prefix_re }.map do |line|
+        line.gsub(prefix_re, "\\1")
+      end.sort(&version_sorter).last
+    end
+
+    def strip_version(version)
+      version.split('.').map(&:to_i).tap do |v|
+        v[2] ||= -1
+      end
     end
   end
 end
